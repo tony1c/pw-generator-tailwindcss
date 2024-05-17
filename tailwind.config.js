3b9a4575ef56fc409855999abcd4e8639d263186
Antonio Mario Cavuto <tony@Antonios-MBP.homenet.telecomitalia.it>
2024-05-18 00:29:08 +0200

    added base-600 color + some comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
         300: "hsl(var(--base-300) / <alpha-value>)",
         400: "hsl(var(--base-400) / <alpha-value>)",
         500: "hsl(var(--base-500) / <alpha-value>)",
+        600: "hsl(var(--base-600) / <alpha-value>)",
         900: "hsl(var(--base-900) / <alpha-value>)",
       },
       primary: "hsl(var(--primary) / <alpha-value>)",
