8e6ccae096264eb0083531f760d4969462332771
Antonio Mario Cavuto <tony@Antonios-MBP.homenet.telecomitalia.it>
2024-05-17 08:32:58 +0200

    added color to tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        base: {
+          100: "hsl(var(--base-100) / <alpha-value>)",
+          200: "hsl(var(--base-200) / <alpha-value>)",
+          300: "hsl(var(--base-300) / <alpha-value>)",
+          400: "hsl(var(--base-400) / <alpha-value>)",
+          500: "hsl(var(--base-500) / <alpha-value>)",
+          900: "hsl(var(--base-900) / <alpha-value>)",
+        },
+        primary: "hsl(var(--primary) / <alpha-value>)",
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
