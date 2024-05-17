4bce4901c4d32bf6f9a980a2d7584c3bcd67737f
Antonio Mario Cavuto <tony@Antonios-MBP.homenet.telecomitalia.it>
2024-05-17 09:01:54 +0200

    main icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {
-      colors: {
-        base: {
-          100: "hsl(var(--base-100) / <alpha-value>)",
-          200: "hsl(var(--base-200) / <alpha-value>)",
-          300: "hsl(var(--base-300) / <alpha-value>)",
-          400: "hsl(var(--base-400) / <alpha-value>)",
-          500: "hsl(var(--base-500) / <alpha-value>)",
-          900: "hsl(var(--base-900) / <alpha-value>)",
-        },
-        primary: "hsl(var(--primary) / <alpha-value>)",
+    colors: {
+      base: {
+        100: "hsl(var(--base-100) / <alpha-value>)",
+        200: "hsl(var(--base-200) / <alpha-value>)",
+        300: "hsl(var(--base-300) / <alpha-value>)",
+        400: "hsl(var(--base-400) / <alpha-value>)",
+        500: "hsl(var(--base-500) / <alpha-value>)",
+        900: "hsl(var(--base-900) / <alpha-value>)",
       },
+      primary: "hsl(var(--primary) / <alpha-value>)",
     },
+    extend: {},
   },
   plugins: [require("@tailwindcss/forms")],
 };
